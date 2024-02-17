6a372ae8b1bd3d215199bedba92764f776a5a042
EeshaneeAJ <eeshaneeaj24@gmail.com>
2024-02-17 16:11:03 +0530

    secondcommit

--- login.py
+++ login.py
@@ -3,23 +3,21 @@ from flask_mysqldb import MySQL
 app= Flask(__name__)
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']='952003'
+app.config['MYSQL_PASSWORD']='My_Sql101'
 app.config['MYSQL_DB']='green_wave'
 mysql=MySQL(app)
 @app.route('/',methods=['GET','POST'])
-def hello_world():
+def login():
     if request.method== 'POST':
         userDetails=request.form
         username=userDetails['username']
-        name=userDetails['name']
-        email=userDetails['email']
         password=userDetails['password']
         cur=mysql.connection.cursor()
-        cur.execute("INSERT INTO sign_up(username,name,email,password)VALUES(%s,%s,%s,%s)",(username,name,email,password))
+        cur.execute("INSERT INTO login(username,password)VALUES(%s,%s)",(username,password))
         mysql.connection.commit()
         cur.close()
         return 'success'
-    return render_template('frontend.html')
+    return render_template('login.html')
 if __name__ == '__main__':
     app.run(debug=True)
     
